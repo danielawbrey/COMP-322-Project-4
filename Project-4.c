20ace3049000fb8168383425ce25dd561efcf441
danielawbrey <danielawbrey@gmail.com>
2018-05-05 21:49:39 -0700

    branch_1

--- Project-4.c
+++ Project-4.c
@@ -12,29 +12,23 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
-#include <grp.h>
 
 bool isDir(const char*);
 bool isFile(const char*);
 char* getBaseName(const char*);
 char* getPathName(const char*);
 bool pathIsRelative(const char*);
+void print(const char*, struct stat);
 
 #define true 1
 #define false 0
 
 int main(int argc, char **argv) {
 	
-	int errors, k, perm;
+	int errors, k;
 	struct stat buf;
 	struct passwd *pwd;
-	struct group grp;
-	struct dirent *direntPtr;
-	char *pPtr, *userDir, *filePath, *homePath, *completePath, *temp;	
-	DIR *dirPtr;
-	char date[10];
-	struct tm *time;
-	char cwd[4096];
+	char *homePath, *completePath;	
 
 	if(argc == 1) { printf("No input files found.\n"); }
 	
@@ -66,58 +60,8 @@ int main(int argc, char **argv) {
 				continue;
 			} 
 
-			else if(buf.st_mode & 0400) { 
-		
-	      			if (getcwd(cwd, sizeof(cwd)) != NULL)
-       					fprintf(stdout, "Current working dir: %s\n\n", cwd);
-	
-				printf("Permissions: ");				
-
-				printf( (S_ISDIR(buf.st_mode)) ? "d" : "-");
-		
-				pwd = getpwuid(getuid());
-
-				//dirPtr = opendir(argv[k]);
-
-				//direntPtr = readdir(dirPtr);
-
-				// User permissions 
-				if(pwd->pw_uid == buf.st_uid) {	
-					printf( (buf.st_mode & S_IRUSR) ? "r" : "-");
-					printf( (buf.st_mode & S_IWUSR) ? "w" : "-");
-					printf( (buf.st_mode & S_IXUSR) ? "x" : "-");
-				}
-
-				// Group permissions
-				if(pwd->pw_gid == buf.st_gid) {
-					printf( (buf.st_mode & S_IRGRP) ? "r" : "-");
-					printf( (buf.st_mode & S_IWGRP) ? "w" : "-");
-					printf( (buf.st_mode & S_IXGRP) ? "x" : "-");
-				}
-
-				// Permissions for other users
-				if(direntPtr->d_name == argv[k]) {
-					printf( (buf.st_mode & S_IROTH) ? "r" : "-");				
-					printf( (buf.st_mode & S_IWOTH) ? "w" : "-");
-					printf( (buf.st_mode & S_IXOTH) ? "x" : "-");
-					printf("\n");
-				}
-	
-				printf ("Number of links: %lu \n",buf.st_nlink);
-
-				pwd = getpwuid(buf.st_uid);
-				printf("User name: %s \n", pwd->pw_name);
-		
-				grp = *getgrgid(buf.st_gid);
-				printf("Group name: %s\n", grp.gr_name);
-				
-				printf ("Bytes: %lu \n", buf.st_size);		
-
-				printf("Last modified time: %s", ctime(&buf.st_ctime));
-
-				printf("Read: %s \n", argv[k]);
-
-				printf("\n");
+			else if(buf.st_mode & 0400) { 	
+				print(argv[k], buf);
 			} 
 
 			else { printf("No read\n"); }
@@ -130,47 +74,117 @@ int main(int argc, char **argv) {
 }
 
 bool isDir(const char* target) {
-
 	struct stat statbuf;
-
 	stat(target, &statbuf);
-
 	return !S_ISDIR(statbuf.st_mode);
 }
 
 bool isFile(const char* target) {
-
 	struct stat statbuf;
-
 	stat(target, &statbuf);
-
 	return S_ISREG(statbuf.st_mode);
 }
 
 char* getBaseName(const char* target) {
-	
 	char* basec = strdup(target);
 	char* bname = basename(basec);
-
-	//printf("%s", bname);
-	
+	strcat(bname, "\0");
 	return bname;
 }
 
 char* getPathName(const char* target) {
-
 	char* dirc = strdup(target);
 	char* dname = dirname(dirc);
-
-	//printf("%s", dname);
-	
 	return dname;
 }
 
 bool pathIsRelative(const char* target) {
-
 	if(target[0] == '/' || isFile(target)) { return false; }
-
 	return true;
 }
+
+void print(const char* path, struct stat buf) {
+	struct passwd *pwd;
+	struct group grp;
+	struct dirent *direntPtr;	
+	DIR *dirPtr;
+	char cwd[4096];
+
+	printf("%s\n\n", path);
+
+	dirPtr = opendir(getPathName(path));
+
+	if(dirPtr == NULL) { return; }
+	
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+       		fprintf(stdout, "Current working dir: %s\n\n", cwd);
+	
+	printf("Permissions: ");					
+		
+	pwd = getpwuid(getuid());
+
+	if(pwd->pw_uid == buf.st_uid) {	// User permissions 
+		printf( (S_ISDIR(buf.st_mode)) ? "d" : "-");
+		printf( (buf.st_mode & S_IRUSR) ? "r" : "-");
+		printf( (buf.st_mode & S_IWUSR) ? "w" : "-");
+		printf( (buf.st_mode & S_IXUSR) ? "x" : "-");
+
+		printf( (buf.st_mode & S_IRGRP) ? "r" : "-");
+		printf( (buf.st_mode & S_IWGRP) ? "w" : "-");
+		printf( (buf.st_mode & S_IXGRP) ? "x" : "-");
+		
+		printf( (buf.st_mode & S_IROTH) ? "r" : "-");				
+		printf( (buf.st_mode & S_IWOTH) ? "w" : "-");
+		printf( (buf.st_mode & S_IXOTH) ? "x" : "-");
+		printf("\n");
+	}
+
+	printf ("Number of links: %lu \n",buf.st_nlink);
+
+	pwd = getpwuid(buf.st_uid);
+	printf("User name: %s \n", pwd->pw_name);
+		
+	grp = *getgrgid(buf.st_gid);
+	printf("Group name: %s\n", grp.gr_name);
+				
+	printf ("Bytes: %lu \n", buf.st_size);		
+
+	printf("Last modified time: %s", ctime(&buf.st_ctime));
+
+	printf("Read: %s \n", path);
+
+	printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
